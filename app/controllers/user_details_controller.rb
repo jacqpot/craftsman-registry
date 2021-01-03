fa2fd5019e32edd3517e30a8187d11b8e52e0201
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2021-01-02 18:06:36 -0700

    fix and update craftsman login and user details path

--- app/controllers/user_details_controller.rb
+++ app/controllers/user_details_controller.rb
@@ -4,7 +4,7 @@ class UserDetailsController < ApplicationController
     # GET /userDetails
     # GET /userDetails.json
     def index
-       @user_details = UserDetails.all
+       @user_details = UserDetail.all
     end
     
     # GET /userDetails/1
@@ -14,7 +14,8 @@ class UserDetailsController < ApplicationController
     
     # GET /userDetails/new
     def new
-       @user_details = UserDetails.new
+
+       @user_details = UserDetail.new
     end
     
     # GET /userDetails/1/edit
@@ -24,7 +25,7 @@ class UserDetailsController < ApplicationController
     # POST /userDetails
     # POST /userDetails.json
     def create
-       @user_details = UserDetails.new(user_details_params)
+       @user_details = UserDetail.new(user_details_params)
        
        respond_to do |format|
           if @user_details.save
@@ -67,11 +68,11 @@ class UserDetailsController < ApplicationController
     
     # Use callbacks to share common setup or constraints between actions.
     def set_user_Details
-       @user_details = UserDetails.find(params[:id])
+       @user_details = UserDetail.find(params[:id])
     end
     
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_details_params
-       params.require(:userDetails).permit(:name, :phone_number, :state, :city, :user_id)
+       params.require(:user_detail).permit(:name, :phone_number, :state, :city, :user_id)
     end
  end
