8cb27a024a5c2cb25013790de15239507b04da61
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2021-01-02 19:45:36 -0700

    filled out requests_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_02_225631) do
+ActiveRecord::Schema.define(version: 2021_01_03_023847) do
+
+  create_table "requests", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "user_details", force: :cascade do |t|
     t.string "name"
