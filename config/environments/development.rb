586aa6edfce7da472a2f64c9ec1a02e7d04966dd
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2021-01-02 23:07:27 -0700

    actioon mailer up and running

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   
-  config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'http' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -75,7 +75,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.delivery_method = :smtp 
-
+  
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
