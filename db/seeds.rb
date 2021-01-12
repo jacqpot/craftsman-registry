cdc4a66779a5c28f2b1cb1b2cf05fdadb31076a4
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2021-01-12 10:38:29 -0700

    create seeds foor Skills, based on spreadsheet from client

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Skill.create(title: "Gun Holsters", description: "Make any custom gun holsters")
+Skill.create(title: "Conseal/Carry Holster", description: "Make only conseal/carry gun holsters")
+Skill.create(title: "Upholstery repair", description: "Repair and resurface old furniture")
+Skill.create(title: "Upholstery", description: "Furniture upholstery with leather and fabric")
+Skill.create(title: "Automotive Upholstery", description: "Repair and replace automotive upholstery")
+Skill.create(title: "Leather Tooling", description: "Tools designs on leather - most type of projects")
+Skill.create(title: "Book / Journal Covers", description: "Make slip on covers for journals,  Bibles and other books.")
+Skill.create(title: "Chair Seats", description: "Make tooled chair seats for antique chairs")
+Skill.create(title: "Non-tooled Items", description: "Make non-tooled articles like key fobs, wallets, purses")
+Skill.create(title: "Minimalist Wallets", description: "Non-tooled thin plain wallets")
+Skill.create(title: "Laser Engraving", description: "Laser engraving on veg-tan leathet")
+Skill.create(title: "General Repairs", description: "To belts, bags, shoes, boots")
+Skill.create(title: "sheathes", description: "knives, axes, saws")
+
+
+
+
