665faf77a625a0634e963c0bbf6da37a1e0fc0a1
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2021-01-20 14:02:10 -0700

    added account update to configure permitted perameters in order to get updating working!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_perameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_number, :state, :city, :avalible])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone_number, :state, :city, :avalible])
 
 
     end
